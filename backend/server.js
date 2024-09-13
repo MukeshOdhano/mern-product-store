0e94cf2727a20e04cdbcba0380978280e6c36c6b
Mukesh-SE <mukeshodhano2018@gmail.com>
2024-09-13 11:33:27 +0500

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,26 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
-import path from "path"
+import path from "path";
 import ProductRoutes from "./routes/product.route.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
-const __dirname = path.resolve()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-	
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
-	// singular -id- create, read, update, delete
-	app.use("/api/product", ProductRoutes);
+// singular -id- create, read, update, delete
+app.use("/api/product", ProductRoutes);
 
 app.listen(PORT, () => {
 	connectDB();
